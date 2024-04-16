310b3d77476fda217380f05d346afe30ba76d82f
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-04-16 11:38:25 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,3 +1,13 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
+mkdir -p ./storage
+chown -R 1000:1000 ./storage
+
+cat << EOT >> ./docker.env
+
+STORAGE_DIR='/app/server/storage'
+SERVER_PORT='3001'
+AUTH_TOKEN="${ADMIN_PASSWORD}"
+JWT_SECRET="${ADMIN_PASSWORD}-${ADMIN_PASSWORD}=${ADMIN_PASSWORD}"
+EOT
