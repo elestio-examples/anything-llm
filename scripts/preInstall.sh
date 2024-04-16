4f705afe6f3cf6ec52fcbabf47a9e3164e86e8b9
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-04-16 12:01:26 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -8,6 +8,7 @@ cat << EOT >> ./docker.env
 
 STORAGE_DIR='/app/server/storage'
 SERVER_PORT='3001'
+VECTOR_DB='lancedb'
 AUTH_TOKEN="${ADMIN_PASSWORD}"
 JWT_SECRET="${ADMIN_PASSWORD}-${ADMIN_PASSWORD}=${ADMIN_PASSWORD}"
 EOT
